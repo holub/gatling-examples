641238fad9b4c32eb734d668c54959015fc9b741
Andrei Holub <aholub@ipsoft.com>
2016-03-03 08:48:57 -0500

    demo for the blocking user creation

--- gatling-custom-protocol-demo/src/main/java/com/custom/SayHello.java
+++ gatling-custom-protocol-demo/src/main/java/com/custom/SayHello.java
@@ -6,6 +6,8 @@
 package com.custom;
 
 import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /**
  * The type Say hello.
@@ -18,7 +20,14 @@ public class SayHello {
    * @param printString the print string
    */
   public final void echo(final String printString, Map<?, ?> feed) {
+    UUID uuid = UUID.randomUUID();
+    System.out.println(" -- s: " + uuid);
+    try {
+      TimeUnit.SECONDS.sleep(5);
+    } catch (InterruptedException e) {}
+
     System.out.println("Say:" + printString + " " + feed.get("username"));
+    System.out.println(" -- e: " + uuid);
   }
 
 }
